2f76bac218a1f84109f9698e8411c620b30a0351
Timbelom <timur.belom@gmail.com>
2023-11-02 14:30:51 +0100

    ai summary testing

--- summarize.py
+++ summarize.py
@@ -1,29 +1,25 @@
 "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"
 import openai
 
-# Your OpenAI API key
-api_key = "YOUR_API_KEY"
+def summarize_text_from_file(file_path):
+    with open(file_path, 'r') as file:
+        text = file.read()
 
-# Text you want to summarize
-text_to_summarize = """
-Put your extracted text here.
-This is the text you want to summarize.
-"""
+    # Set the parameters for the API call
+    prompt = f"Summarize the following text: {text}"
+    max_tokens = 100  # Adjust the number of tokens as needed for the desired summary length
 
-# Define the parameters for the GPT-3 API request
-response = openai.Completion.create(
-    engine="davinci",
-    prompt=f"Summarize the following text:\n{text_to_summarize}\n\nSummary:",
-    max_tokens=50,  # Adjust this as needed for your desired summary length
-    api_key=api_key
-)
+    # Make an API call to generate the summary
+    response = openai.Completion.create(
+        engine="text-davinci-002",  # You can choose a different engine depending on your subscription
+        prompt=prompt,
+        max_tokens=max_tokens
+    )
 
-# Extract the generated summary
-summary = response.choices[0].text.strip()
+    summary = response.choices[0].text
+    return summary
 
-# Print or save the summary
-print("Generated Summary:", summary)
-
-# You can save the summary to a file if needed
-with open('summary.txt', 'w', encoding='utf-8') as file:
-    file.write(summary)
+# Example usage
+file_path = 'text.txt'
+summary = summarize_text_from_file(file_path)
+print(summary)
